4addfbb6783db689e64db5a115a1edd30aa2c9e3
Col-Ian <100207902+Col-Ian@users.noreply.github.com>
2023-04-11 12:20:21 -0230

    Added Controller for java model w/ add and getAll

---
+++ src/main/java/com/Sprint2/Team2/controller/VideoGamesController.java
@@ -0,0 +1,26 @@
+package com.Sprint2.Team2.controller;
+
+import com.Sprint2.Team2.model.VideoGames;
+import com.Sprint2.Team2.service.VideoGamesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/videogames")
+public class VideoGamesController {
+    @Autowired
+    public VideoGamesService videoGamesService;
+
+    @PostMapping("/add")
+    public String add(@RequestBody VideoGames videoGames){
+        videoGamesService.saveVideoGames(videoGames);
+        return "New game added";
+    }
+
+    @GetMapping("/getAll")
+    public List<VideoGames> getAllVideoGames(){
+        return videoGamesService.getAllVideoGames();
+    }
+}
