dde176d75b07619d07736acec1dd3eb11265ea70
Col-Ian <100207902+Col-Ian@users.noreply.github.com>
2023-04-12 10:28:05 -0230

    Added CrossOrigin

--- src/main/java/com/Sprint2/Team2/controller/VideoGamesController.java
+++ src/main/java/com/Sprint2/Team2/controller/VideoGamesController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/videogames")
+@CrossOrigin
 public class VideoGamesController {
     @Autowired
     public VideoGamesService videoGamesService;
