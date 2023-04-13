b0e309e24939159a9b63d98b8468b0296a662612
William <williamstamp88@gmail.com>
2023-04-13 20:06:16 -0230

    Fuzzy search implemented

--- src/main/java/com/Sprint2/Team2/repository/VideoGamesRepository.java
+++ src/main/java/com/Sprint2/Team2/repository/VideoGamesRepository.java
@@ -3,13 +3,15 @@ package com.Sprint2.Team2.repository;
 import com.Sprint2.Team2.model.VideoGames;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 import java.util.List;
 
 @Repository
 public interface VideoGamesRepository extends JpaRepository<VideoGames, Long> {
     VideoGames getById(Long id);
-    List<VideoGames> findByName(String name);
+    Optional<VideoGames> findByName(String name);
+
     List<VideoGames> findByGenre(String genre);
 
     List<VideoGames> findByPlatforms(String platforms);
--- src/main/java/com/Sprint2/Team2/service/VideoGamesImplementation.java
+++ src/main/java/com/Sprint2/Team2/service/VideoGamesImplementation.java
@@ -2,8 +2,32 @@ package com.Sprint2.Team2.service;
 
 import com.Sprint2.Team2.model.VideoGames;
 import com.Sprint2.Team2.repository.VideoGamesRepository;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.TextField;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.Sprint2.Team2.model.VideoGames;
+import com.Sprint2.Team2.repository.VideoGamesRepository;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.apache.lucene.queryparser.classic.ParseException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -28,9 +52,59 @@ public class VideoGamesImplementation implements VideoGamesService{
         return videoGamesRepository.getById(id);
     }
 
+
     @Override
-    public List<VideoGames> getByName(String name){
-        return videoGamesRepository.findByName(name);
+    public List<VideoGames> getByName(String name) {
+        List<VideoGames> videoGames = videoGamesRepository.findAll();
+        System.out.println("Total video games: " + videoGames.size());
+        List<VideoGames> searchResults = new ArrayList<>();
+
+        // Set up the analyzer, index, and writer.
+        StandardAnalyzer analyzer = new StandardAnalyzer();
+        Directory index = new RAMDirectory();
+        IndexWriterConfig config = new IndexWriterConfig(analyzer);
+        IndexWriter writer;
+        try {
+            writer = new IndexWriter(index, config);
+
+            // Add your video games to the index.
+            for (VideoGames videoGame : videoGames) {
+                Document doc = new Document();
+                // Assuming you have a `name` field in your VideoGame class.
+                doc.add(new TextField("name", videoGame.getName(), Field.Store.YES));
+                writer.addDocument(doc);
+                System.out.println("Indexed: " + videoGame.getName()); // Add this line
+            }
+            writer.close();
+
+            // Perform the fuzzy search.
+            Query query = new QueryParser("name", analyzer).parse(name + "~");
+            DirectoryReader reader = DirectoryReader.open(index);
+            IndexSearcher searcher = new IndexSearcher(reader);
+            TopDocs docs = searcher.search(query, videoGames.size());
+
+            // Collect the search results.
+            for (ScoreDoc scoreDoc : docs.scoreDocs) {
+                Document doc = searcher.doc(scoreDoc.doc);
+                String foundName = doc.get("name");
+                // Assuming you have a method to find a video game by name.
+                VideoGames foundVideoGame = findVideoGameByName(foundName);
+                if (foundVideoGame != null) {
+                    System.out.println("Found: " + foundVideoGame.getName());
+                    searchResults.add(foundVideoGame);
+                }
+            }
+
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+
+        return searchResults;
+    }
+
+    private VideoGames findVideoGameByName(String foundName) {
+        return videoGamesRepository.findByName(foundName).orElse
+                (null);
     }
 
     @Override
